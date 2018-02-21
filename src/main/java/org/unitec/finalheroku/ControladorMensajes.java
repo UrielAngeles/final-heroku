928b88114ea47036f7449b065c95abf1195c304f
UriDrack7 <UriDrack7@10.250.0.76>
2018-02-20 21:46:52 -0600

    Registros en MongoDB

--- src/main/java/org/unitec/finalheroku/ControladorMensajes.java
+++ src/main/java/org/unitec/finalheroku/ControladorMensajes.java
@@ -6,6 +6,10 @@
 package org.unitec.finalheroku;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.annotation.Id;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,12 +22,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class ControladorMensajes {
+    @Autowired RepositorioMensajito repoMensa;
     
-    @RequestMapping(value="/mensajito", method=RequestMethod.GET,headers=("Accept=application/json"))
-    public Mensajito hola(){
-        Mensajito mensa=new Mensajito("Hola","Me asustan los temblores!");
+    @RequestMapping(value = "/mensajito",method = RequestMethod.GET,
+            headers = {"Accept=application/json"})
+    public ArrayList<Mensajito> hola(){
+      
+        return(ArrayList<Mensajito>) repoMensa.findAll();
         
-        return mensa;
     }
     
+    @RequestMapping(value="/mensajito/{Id}/{titulo}/{cuerpo}",method=RequestMethod.GET,
+            headers={"Accept=application/json"})
+    
+    public Estatus guardar(@PathVariable String Id,@PathVariable String titulo, @PathVariable String cuerpo){
+        repoMensa.save(new Mensajito(Id, titulo,cuerpo));
+        return new Estatus(true, "Guardado con exito");
+    }
 }
--- src/main/java/org/unitec/finalheroku/Mensajito.java
+++ src/main/java/org/unitec/finalheroku/Mensajito.java
@@ -5,14 +5,35 @@
  */
 package org.unitec.finalheroku;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import org.springframework.data.annotation.Id;
+
 /**
  *
  * @author UriDrack7
  */
 public class Mensajito {
     
+    @Id
+    
+    private String Id;   
     private String titulo;
     private String Cuerpo;
+    private LocalDate fecha;
+    private LocalTime time;
+
+    public Mensajito(String Id, String titulo, String Cuerpo) {
+        this.Id = Id;
+        this.titulo = titulo;
+        this.Cuerpo = Cuerpo;
+        fecha=LocalDate.now();
+        time=LocalTime.now();
+    }
+
+    public Mensajito(String Id) {
+        this.Id = Id;
+    }
 
     public String getTitulo() {
         return titulo;
@@ -36,6 +57,32 @@ public class Mensajito {
     public Mensajito(String titulo, String Cuerpo) {
         this.titulo = titulo;
         this.Cuerpo = Cuerpo;
+        fecha=LocalDate.now();
+        time=LocalTime.now();
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String Id) {
+        this.Id = Id;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
     }
     
 }
